0b62b9b4e9b7b3cb3f4fb6dc84568365956ee882
Bobby Vandiver <bobby.vandiver88@gmail.com>
2013-10-27 16:53:31 -0400

    Provision VM with PHPUnit

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,4 +10,11 @@ file { "/var/www":
     ensure => link,
     target => "/vagrant",
     force => true,
+    recurse => true,
+}
+
+file { "/usr/local/bin/phpunit":
+    ensure => link,
+    target => "/vagrant/lib/phpunit.phar",
+    force => true,
 }
