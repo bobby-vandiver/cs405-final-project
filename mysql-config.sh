e7dd9a4e85e6e6d62593135e761f4a27a41c7dee
Bobby Vandiver <bobby.vandiver88@gmail.com>
2013-11-03 20:25:41 -0500

    Minor cleanup of mysql-config script

--- mysql-config.sh
+++ mysql-config.sh
@@ -2,7 +2,3 @@
 
 mysqladmin -u root password 'mysql'
 mysql --user=root --password=mysql --host=localhost --port=3306 < /vagrant/grant-remote-access.sql
-
-#sudo cp /vagrant/my.cnf /etc/mysql/
-
-#sudo /etc/init.d/mysql restart
