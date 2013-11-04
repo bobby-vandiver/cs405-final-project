1086c7328dca341f60f9e54ffba5a3d1f57f63c2
Bobby Vandiver <bobby.vandiver88@gmail.com>
2013-11-03 20:19:46 -0500

    Add create and drop table sql scripts

---
+++ sql-queries/create-tables.sql
@@ -0,0 +1,56 @@
+CREATE TABLE Users (
+    username        VARCHAR(50)     NOT NULL,
+    password        VARCHAR(50),
+    role            INT,
+
+    houseNumber     INT,
+    street          VARCHAR(50),
+    city            VARCHAR(50),
+    state           CHAR(2),
+    zip             CHAR(5),
+
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE Orders (
+    orderId         INT         NOT NULL,
+    status          INT,
+    time            TIMESTAMP,
+    total           DECIMAL(10, 2),
+    username        VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY (orderId),
+    FOREIGN KEY (username) REFERENCES Users(username)
+);
+
+CREATE TABLE Items (
+    isbn            CHAR(10)    NOT NULL,
+    quantity        INT,
+    price           DECIMAL(10, 2),
+    type            INT,
+    name            VARCHAR(50),
+    promotion       REAL,
+
+    PRIMARY KEY (isbn)
+);
+
+CREATE TABLE OrderItems (
+    orderId         INT         NOT NULL,
+    isbn            CHAR(10)    NOT NULL,
+    quantity        INT,
+    salePrice       DECIMAL(10, 2),
+    
+    PRIMARY KEY (orderId, isbn),
+    FOREIGN KEY (orderId) REFERENCES Orders(orderId),
+    FOREIGN KEY (isbn) REFERENCES Items(isbn)
+);
+
+CREATE TABLE BrowsingHistory (
+    username        VARCHAR(50)     NOT NULL,
+    isbn            CHAR(10)        NOT NULL,
+    views           INT,
+
+    PRIMARY KEY (username, isbn),
+    FOREIGN KEY (username) REFERENCES Users(username),
+    FOREIGN KEY (isbn) REFERENCES Items(isbn)
+);
---
+++ sql-queries/drop-tables.sql
@@ -0,0 +1,5 @@
+DROP TABLE BrowsingHistory;
+DROP TABLE OrderItems;
+DROP TABLE Items;
+DROP TABLE Orders;
+DROP TABLE Users;
