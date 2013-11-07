8c5d832e321b638294fcbcecbdc88a4a82ce6215
Bobby Vandiver <bobby.vandiver88@gmail.com>
2013-11-06 21:24:50 -0500

    Snake case function name

--- app/database.php
+++ app/database.php
@@ -1,5 +1,5 @@
 <?php
-    function createConnection() {
+    function create_connection() {
         $config = parse_ini_file("database.ini");
     
         // Get the connection params
@@ -15,7 +15,7 @@
     // Utility function to perform a query so this is
     // the only script that talks to the database
     function execute_query($sql) {
-        $connection = createConnection();
+        $connection = create_connection();
         if($connection) {
             return mysqli_query($connection, $sql);
         }
