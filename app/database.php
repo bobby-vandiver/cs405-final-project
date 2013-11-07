7f47f67483f0cf07c733bbbd7b7ac5d7cf86a4ef
Bobby Vandiver <bobby.vandiver88@gmail.com>
2013-11-06 20:35:12 -0500

    Refactor connection into factory method

--- app/database.php
+++ app/database.php
@@ -1,20 +1,23 @@
 <?php
-    $config = parse_ini_file("database.ini");
-
-    // Get the connection params
-    $username = $config['username'];
-    $password = $config['password'];
-    $database = $config['database'];
-    $host = $config['host'];
-    $port = $config['port'];
-
-    $database_connection = mysqli_connect($host, $username, $password, $database, $port);
+    function createConnection() {
+        $config = parse_ini_file("database.ini");
+    
+        // Get the connection params
+        $username = $config['username'];
+        $password = $config['password'];
+        $database = $config['database'];
+        $host = $config['host'];
+        $port = $config['port'];
+    
+        return mysqli_connect($host, $username, $password, $database, $port);
+    }
 
     // Utility function to perform a query so this is
     // the only script that talks to the database
     function execute_query($sql) {
-        if($database_connection) {
-            return mysqli_query($database_connection, $sql);
+        $connection = createConnection();
+        if($connection) {
+            return mysqli_query($connection, $sql);
         }
     }
 ?>
