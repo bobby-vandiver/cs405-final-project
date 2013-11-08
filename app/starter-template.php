c6bd56e1b4a933c1a4a99e36b0f010b63a5b07ba
Bobby Vandiver <bobby.vandiver88@gmail.com>
2013-11-07 19:48:38 -0500

    Test PHP with bootstrap

--- app/starter-template.php
+++ app/starter-template.php
@@ -56,6 +56,8 @@
       <h1>Bootstrap starter template</h1>
       <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
 
+      <?php echo "<p>This is some hot PHP generated text.</p>" ?>
+
     </div> <!-- /container -->
 
     <!-- Le javascript
